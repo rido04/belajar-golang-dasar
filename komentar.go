666f0a78f2d90c8815e0a3c4419c9037ae1530ac
Ridho Febrian <ridhofebrian.445@gmail.com>
2025-03-25 21:14:13 +0700

    belajar golang dasar

---
+++ komentar.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+// komentar terbaik pada kode adalah kode itu sendiri
+// saat membuat kode. kita perlu membuat kode tsb semudah mungkin untuk di baca
+// maka dari itu di butuhkan komentar / comment
+// untuk menjelaskan kode yang sulit untuk di jelaskan
+// ada beberapa cara untuk membuat komentar
+// menggunakan double slash untuk single line
+
+/*
+ini adalah komentar
+untuk lebih dari dua baris
+atau multiline comment
+*/
+
+func main() {
+	fmt.Println()
+}
