05a0ea881ea1f547d4846a3c8143ab99b5d39951
Ridho Febrian <ridhofebrian.445@gmail.com>
2025-03-26 14:48:23 +0700

    belajar golang dasar

--- nil.go
+++ nil.go
@@ -18,11 +18,100 @@ func NewMap(name string) map[string]string{
 	}
 }
 
+// contoh dengan menggunakan slice
+func slice(condition bool) []int {
+	if condition {
+		return nil
+	} else {
+		return []int{1, 2, 3}
+	}
+}
+
+// contoh menggunakan pointer
+func pointer(condition bool) *int{
+	if condition{
+		return nil
+	} else {
+		value := 42
+		return	&value
+	}
+}
+
+// contoh menggunakan interface
+func NewInterface(condition bool) any {
+	if condition {
+		return nil
+	} else {
+		return "Hello World!"
+	}
+}
+
+// conoth menggunakan function
+func NewFunction(condition bool) func(){
+	if condition {
+		return nil
+	} else {
+		return func() {
+			fmt.Println("Haloo, ini dari function")
+		}
+	}
+}
+
+// contoh menggunakan channel
+func NewChannel(condition bool) chan int{
+	if condition {
+		return nil
+	} else {
+		return make(chan int)
+	}
+}
+
 func main(){
+	// implementasi tipe data map
 	data := NewMap("")
 	if data == nil{
 		fmt.Println("Data Kosong")
 	} else {
 		fmt.Println(data["name"])
+	}
+
+	// implementasi tipe data slice
+	data2:= slice(true)
+	if data2 == nil {
+		fmt.Println("Data Slice Kosong")
+	} else {
+		fmt.Println(data2)
+	}
+
+	// implementasi tipe data pointer
+	data3 := pointer(true) 
+		if data3 == nil {
+			fmt.Println("Pointernya kosong")
+		} else {
+			fmt.Println(*data3)
+		}
+	
+	// implementasi tipe data channel
+	data4 := NewChannel(true)
+	if data4 == nil {
+		fmt.Println("Channel kosong")
+	} else {
+		fmt.Println("channel dibuat")
+	}
+
+	// implementasi interface
+	data5 := NewInterface(true)
+	if data5 == nil {
+		fmt.Println("Interface Kosong")
+	} else {
+		fmt.Println(data5)
+	}
+
+	// implementasi function
+	data6 := NewFunction(true)
+	if data6 == nil {
+		fmt.Println("Function Kosong")
+	} else {
+		fmt.Print(data6)
 	}
 }
