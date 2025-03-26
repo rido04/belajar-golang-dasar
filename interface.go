fa5ea02d5365ea1c53dc352233f8a373f71f6756
Ridho Febrian <ridhofebrian.445@gmail.com>
2025-03-26 14:18:36 +0700

    belajar golang dasar

--- interface.go
+++ interface.go
@@ -37,12 +37,43 @@ func (person Person) GetName()string{
 	return person.Name
 }
 
+// jadi semua implementasi dari hasName bisa kita kirim lewat parameter di atas
 func (person Person) GetAddress()string{
 	return person.Address
 }
 
-// jadi semua implementasi dari hasName bisa kita kirim lewat parameter di atas
+// adapun interface kosong
+// biasanya di daam OOP ada satu data parent di puncak ynag bisa dianggap sebagai semua implementasi data yang ada di bahasa tersebut
+// contoh ada di java.lang.object dan oop php
+// dalam kasus ini, golang menggunakan interface kosong
+// ini adalah interface yang ridak memiliki deklarasi method satupun, hal ini secara otomatis membuat semua tipe data akan menjadi implementasi nya
+// interface kosong juga memiliki type alias bernama any
+
+
+// contoh penggunaa interface kosong
+// fmt.Println(a...interface{})
+// panic(v...interface{})
+// recover() interface{}
+// dan lain lain
+// dan sebenernya semua itu adalah interface kosong, artinya bisa dikirim data apapaun, benar benar bisa kirim data apapun bahkan sampai struct dan interface
+
+// conoth interface kosong
+func Ups() interface{}{
+	// return 1
+	// return true
+	return "Ups"
+	// bisa kembalikan nilai int, boolean, string, dlll. pokoknya bebas
+}
+
+// jika menggunakan any(sama aja, cuman lebih simpel)
+func oops() any {
+	return "Lagi belajar golang dasar nich, senggol dong"
+}
 func main(){
 	person := Person{Name: "Ridho Febrian", Address: "Pakulonan Barat"}
 	SayHello(person)
+	kosong := Ups()
+	fmt.Println(kosong)
+	kosong2 := oops()
+	fmt.Println(kosong2)
 }
