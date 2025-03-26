71f7620b2cd4ea2c72a61c5f3884176f7f9c474c
Ridho Febrian <ridhofebrian.445@gmail.com>
2025-03-26 14:50:13 +0700

    belajar golang dasar

---
+++ type_assertions.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+// type assertions adalah kemampuan merubah tupe data menjadi tipe data yang kita inginkan
+// fitur ini seringkali digunakan ketika kita bertemu dengan tipe data interface kosong
+func main(){
+	fmt.Println()
+}
