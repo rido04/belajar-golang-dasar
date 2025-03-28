fabac9f234c1a92b32886b4f1a771d2fd390301b
Ridho Febrian <ridhofebrian.445@gmail.com>
2025-03-28 14:09:08 +0700

    belajar golang dasar

---
+++ helper/helper.go
@@ -0,0 +1,15 @@
+package helper
+
+// nama package harus disesuaikan dengan nama fodler
+
+// PACKAGE
+// adalah tempat yang bisa digunakan untuk mengatur kode program yang kita buat di golang
+// dengan menggunakan package, kita bisa lebih merapihkan kode yang kita buat
+// package sendiri sebenernya cuma direktori folder di sistem operasi kita
+// contoh untuk package main
+// berarti dia kode yang harus di jalankan karena MAIN
+
+func SayHello(name string)string{
+	return "Hello" + name
+}
+
---
+++ import.go
@@ -0,0 +1,16 @@
+package main
+
+// IMPORT
+// secara standar, file golang hanya bisa mengakses file golang lainnya yang berada di dalam package yang sama
+// jika kita mau akses file golang yang berada di package yang berbeda, kita bisa pakai import
+import (
+	// pastikan direktori nya benar, bisa cek di file "go.mod"
+	"belajar-golang-dasar/helper"
+	"fmt"
+)
+
+func main(){
+	// lalu panggil function yang ada di file helped tadi yang sudah di import
+	result := helper.SayHello("Muhammad Ridho Febrian")
+	fmt.Println(result)
+}
